3d9e2ea1a87913c85f5e20e4cd80b6a022f64efd
Kârūn The Rich <development@xpowermine.com>
2022-11-07 02:27:44 +0300

    XPower: solidity@0.8.17

--- hardhat.config.js
+++ hardhat.config.js
@@ -104,7 +104,7 @@ task("mine", "Mines for XPower tokens")
  */
 module.exports = {
   solidity: {
-    version: "0.8.16",
+    version: "0.8.17",
     settings: {
       optimizer: {
         enabled: true,
@@ -129,7 +129,7 @@ module.exports = {
         "0x86f78c5416151fe3546dece84fda4b4b1e36089f2dbc48496faf3a950f16157c",
         "0x750839e9dbbd2a0910efe40f50b2f3b2f2f59f5580bb4b83bd8c1201cf9a010a",
       ],
-      gasMultiplier: 1.125, // gasPrice: 100_000_000_000,
+      gasMultiplier: 1.25, // gasPrice: 100_000_000_000,
     },
     /* avalanche */ fuji: {
       url: "https://api.avax-test.network/ext/bc/C/rpc",
@@ -138,7 +138,7 @@ module.exports = {
         process.env.MINT_ADDRESS_PK ||
           "0x0000000000000000000000000000000000000000000000000000000000000000",
       ],
-      gasMultiplier: 1.125, // gasPrice: 100_000_000_000,
+      gasMultiplier: 1.25, // gasPrice: 100_000_000_000,
     },
     /* avalanche */ mainnet: {
       url: "https://api.avax.network/ext/bc/C/rpc",
@@ -147,7 +147,7 @@ module.exports = {
         process.env.MINT_ADDRESS_PK ||
           "0x0000000000000000000000000000000000000000000000000000000000000000",
       ],
-      gasMultiplier: 1.125, // gasPrice: 100_000_000_000,
+      gasMultiplier: 1.25, // gasPrice: 100_000_000_000,
     },
   },
   gasReporter: {
