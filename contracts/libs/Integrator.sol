d932415d4ece1faaa49400362e63f67b748d0ada
Kârūn The Rich <development@xpowermine.com>
2023-02-06 16:57:53 +0300

    XPower: (stamp, value)-integrator

---
+++ contracts/libs/Integrator.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+/**
+ * Allows to integrate over an array of (stamp, value) tuples, and to take
+ * the duration i.e. Δ-stamp weighted arithmetic mean of those values.
+ */
+library Integrator {
+    struct Item {
+        /** stamp of value */
+        uint256 stamp;
+        /** value of interest */
+        uint256 value;
+        /** cumulative sum over Δ-stamps × values */
+        uint256 area;
+    }
+
+    /** @return head item */
+    function headOf(Item[] storage items) internal view returns (Item memory) {
+        return items.length > 0 ? items[0] : Item(0, 0, 0);
+    }
+
+    /** @return last item */
+    function lastOf(Item[] storage items) internal view returns (Item memory) {
+        return items.length > 0 ? items[items.length - 1] : Item(0, 0, 0);
+    }
+
+    /** @return next item (for stamp and value) */
+    function _nextOf(Item[] storage items, uint256 stamp, uint256 value) private view returns (Item memory) {
+        if (items.length > 0) {
+            Item memory last = items[items.length - 1];
+            require(stamp >= last.stamp, "invalid stamp");
+            uint256 area = value * (stamp - last.stamp);
+            return Item(stamp, value, last.area + area);
+        }
+        return Item(stamp, value, 0);
+    }
+
+    /** @return Δ-stamp weighted arithmetic mean of values (incl. next stamp and value) */
+    function meanOf(Item[] storage items, uint256 stamp, uint256 value) internal view returns (uint256) {
+        uint256 area = areaOf(items, stamp, value);
+        Item memory head = headOf(items);
+        if (stamp > head.stamp) {
+            return area / (stamp - head.stamp);
+        }
+        return head.value;
+    }
+
+    /** @return area of Δ-stamps × values (incl. next stamp and value) */
+    function areaOf(Item[] storage items, uint256 stamp, uint256 value) internal view returns (uint256) {
+        return _nextOf(items, stamp, value).area;
+    }
+
+    /** append (stamp, value) to items (with stamp >= last?.stamp) */
+    function append(Item[] storage items, uint256 stamp, uint256 value) internal {
+        items.push(_nextOf(items, stamp, value));
+    }
+}
---
+++ contracts/test/IntegratorTest.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import {Integrator} from "../libs/Integrator.sol";
+
+contract IntegratorTest {
+    Integrator.Item[] public items;
+
+    function headOf() public view returns (Integrator.Item memory) {
+        return Integrator.headOf(items);
+    }
+
+    function lastOf() public view returns (Integrator.Item memory) {
+        return Integrator.lastOf(items);
+    }
+
+    function meanOf(uint256 stamp, uint256 value) public view returns (uint256) {
+        return Integrator.meanOf(items, stamp, value);
+    }
+
+    function areaOf(uint256 stamp, uint256 value) public view returns (uint256) {
+        return Integrator.areaOf(items, stamp, value);
+    }
+
+    function append(uint256 stamp, uint256 value) public {
+        Integrator.append(items, stamp, value);
+    }
+}
