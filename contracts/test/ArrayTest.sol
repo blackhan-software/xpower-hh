a822b1fd1bd396d081169039ab1d7225f5acc66c
Kârūn The Rich <development@xpowermine.com>
2023-05-21 21:31:09 +0300

    XPower: Array.{sorted,unique} tests

---
+++ contracts/test/ArrayTest.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import {Array} from "../libs/Array.sol";
+
+contract ArrayTest {
+    function sorted(uint256[] memory array) public pure returns (bool) {
+        return Array.sorted(array);
+    }
+
+    function unique(uint256[] memory array) public pure returns (bool) {
+        return Array.unique(array);
+    }
+}
