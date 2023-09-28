e6dd9db61dbb4123ad989045dab4ccda3c9a5b83
Kârūn The Rich <development@xpowermine.com>
2023-09-28 04:44:53 +0300

    XPower: HH.gas-multiplier=1.0

--- hardhat.config.js
+++ hardhat.config.js
@@ -120,7 +120,7 @@ module.exports = {
         "0x86f78c5416151fe3546dece84fda4b4b1e36089f2dbc48496faf3a950f16157c",
         "0x750839e9dbbd2a0910efe40f50b2f3b2f2f59f5580bb4b83bd8c1201cf9a010a",
       ],
-      gasMultiplier: 1.25, // gasPrice: 100_000_000_000,
+      gasMultiplier: 1.0, // gasPrice: 100_000_000_000,
     },
     /* avalanche */ fuji: {
       url:
@@ -131,7 +131,7 @@ module.exports = {
         process.env.MINT_ADDRESS_PK ||
           "0x0000000000000000000000000000000000000000000000000000000000000000",
       ],
-      gasMultiplier: 1.25, // gasPrice: 100_000_000_000,
+      gasMultiplier: 1.0, // gasPrice: 100_000_000_000,
     },
     /* avalanche */ mainnet: {
       url:
@@ -141,7 +141,7 @@ module.exports = {
         process.env.MINT_ADDRESS_PK ||
           "0x0000000000000000000000000000000000000000000000000000000000000000",
       ],
-      gasMultiplier: 1.25, // gasPrice: 100_000_000_000,
+      gasMultiplier: 1.0, // gasPrice: 100_000_000_000,
     },
   },
   etherscan: {
